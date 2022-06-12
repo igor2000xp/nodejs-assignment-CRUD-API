a08e7381e72bc320161a2a686a32dd1c9de9ff99
igor2000xp <igorring@gmail.com>
2022-06-12 22:03:06 +0300

    refactor: remove some commented code

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,18 +1,8 @@
 import * as fs from 'fs/promises';
-// import * as os from "os";
-// import { fileURLToPath } from "url";
-// import * as path from 'path';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const PATH_FILES = 'files';
 const ERROR_FS = 'FS operation failed';
 
 export const list = async (path) => {
   try {
-    // console.log(filesArr);
-    // const dirList = await fs.readdir(path);
-    // return dirList.join(os.EOL);
     return await fs.readdir(path);
 
   } catch (err) {
